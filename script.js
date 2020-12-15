545a4b90101b302af46bdeacf4968e5f33c21212
Jude <jantungkuno@gmail.com>
2020-12-14 23:30:36 -0500

    created global variables and local storage functions

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-// Script //
+// Create global variables
+var cityList = [];
+var cityname;
+// Create local storage functions
+initCityList();
+initWeather();
+// Create function to search for the city searched by the user
