a129a7cc699ae0e4bc17bfbc27f654a463ebea0b
Jude <jantungkuno@gmail.com>
2020-12-14 23:35:59 -0500

    Create function to search for city input

--- script.js
+++ script.js
@@ -4,4 +4,16 @@ var cityname;
 // Create local storage functions
 initCityList();
 initWeather();
-// Create function to search for the city searched by the user
+// Create function to search for the city inputted by the user
+function renderCities(){
+    $("#cityList").empty();
+    $("#cityInput").val("");
+    
+    for (i=0; i<cityList.length; i++){
+        var a = $("<a>");
+        a.addClass("list-group-item list-group-item-action list-group-item-primary city");
+        a.attr("data-name", cityList[i]);
+        a.text(cityList[i]);
+        $("#cityList").prepend(a);
+    } 
+}
