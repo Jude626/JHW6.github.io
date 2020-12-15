ba6a15e0c70a9b53cde69421205bb5869e746cd0
Jude <jantungkuno@gmail.com>
2020-12-15 00:20:18 -0500

    created weather display for main body

--- script.js
+++ script.js
@@ -70,21 +70,37 @@ async function displayWeather() {
       })
         console.log(response);
         // Create div for main body
-      
+        var currentWeatherDiv = $("<div class='card-body' id='currentWeather'>");
+        var getCurrentCity = response.name;
         // Create current date
-     
+        var date = new Date();
+        var val=(date.getMonth()+1)+"/"+date.getDate()+"/"+date.getFullYear();
         // Create current weather icon
-        
+        var getCurrentWeatherIcon = response.weather[0].icon;
+        var displayCurrentWeatherIcon = $("<img src = http://openweathermap.org/img/wn/" + getCurrentWeatherIcon + "@2x.png />");
         // Create current city and append it
-        
+        var currentCityEl = $("<h3 class = 'card-body'>").text(getCurrentCity+" ("+val+")");
+        currentCityEl.append(displayCurrentWeatherIcon);
+        currentWeatherDiv.append(currentCityEl);
         // Create variable for temperature
-
+        var getTemp = response.main.temp.toFixed(1);
         // get temperature and add ° F, append it
-        
+        var tempEl = $("<p class='card-text'>").text("Temperature: "+getTemp+"° F");
+        currentWeatherDiv.append(tempEl);
+        var getHumidity = response.main.humidity;
         // get humidity and add %, append it
-        
+        var humidityEl = $("<p class='card-text'>").text("Humidity: "+getHumidity+"%");
+        currentWeatherDiv.append(humidityEl);
         // get wind speed in mph, and append it
-        
+        var getWindSpeed = response.wind.speed.toFixed(1);
+        var windSpeedEl = $("<p class='card-text'>").text("Wind Speed: "+getWindSpeed+" mph");
+        currentWeatherDiv.append(windSpeedEl);
         // add latitude & longitude variables
-       
+        var getLong = response.coord.lon;
+        var getLat = response.coord.lat;
         // add api for latitude/longitude coordinates for the city searched
+        var uvURL = "https://api.openweathermap.org/data/2.5/uvi?appid=d3b85d453bf90d469c82e650a0a3da26&lat="+getLat+"&lon="+getLong;
+        var uvResponse = await $.ajax({
+            url: uvURL,
+            method: "GET"
+        })
